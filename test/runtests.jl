af3a8395a616dc8da012c076e8661b828c22ed6c
Federico Stra <stra.federico@gmail.com>
2021-04-02 19:44:27 +0200

    Improve tests.
    Actually test the stderr of the `@dbg` macro.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,23 @@
-using Dbg
 using Test
+using Suppressor
 
-@testset "Dbg.jl" begin
-    # currently, this is not testing any functionality, only successful compilation
+using Dbg
+
+function foo()
     m = [1 2; 3 4]
     @dbg 1+2 "Hello" m
+    42
+end
+
+expected_foo_err = """
+Main:runtests.jl:8  1 + 2 = 3
+Main:runtests.jl:8  "Hello" = "Hello"
+Main:runtests.jl:8  m = [1 2; 3 4]
+"""
+
+@testset "Dbg.jl" begin
+    local result
+    err = @capture_err result = foo()
+    @test result == 42
+    @test err == expected_foo_err
 end
