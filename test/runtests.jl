e11b683680b0e1575d96760369f24384691a229b
Federico Stra <stra.federico@gmail.com>
2021-04-03 14:58:36 +0200

    Refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,7 @@
 using Test
-using Suppressor
+using SafeTestsets
+using TestSetExtensions
 
-using DbgMacro
-
-function foo()
-    m = [1 2; 3 4]
-    @dbg 1+2 "Hello" m
-    42
-end
-
-expected_foo_err = """
-Main:runtests.jl:8  1 + 2 = 3
-Main:runtests.jl:8  "Hello" = "Hello"
-Main:runtests.jl:8  m = [1 2; 3 4]
-"""
-
-@testset "DbgMacro.jl" begin
-    local result
-    err = @capture_err result = foo()
-    @test result == 42
-    @test err == expected_foo_err
+@testset ExtendedTestSet "DbgMacro.jl" begin
+    @safetestset Simple = "simple" begin include("simple.jl") end
 end
---
+++ test/simple.jl
@@ -0,0 +1,19 @@
+using DbgMacro
+
+function foo()
+    m = [1 2; 3 4]
+    @dbg 1+2 "Hello" m
+    42
+end
+
+expected_foo_err = """
+Main.Simple:simple.jl:5  1 + 2 = 3
+Main.Simple:simple.jl:5  "Hello" = "Hello"
+Main.Simple:simple.jl:5  m = [1 2; 3 4]
+"""
+
+using Suppressor: @capture_err
+
+err = @capture_err global result = foo()
+@test result == 42
+@test err == expected_foo_err
